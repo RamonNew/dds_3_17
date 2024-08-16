2964d544dffb0b249eb8465dd5a147b56f8782e7
Ramon de Holanda <ramonhnascimento@gmail.com>
2024-08-15 21:01:08 -0300

    lading page

---
+++ 08_15/landing_page/assets/js/app.js
@@ -0,0 +1,13 @@
+// Hamburger menu selections
+
+// scroll-to-top selection
+
+// Hamburger menu functionality
+
+// Theme switcher selection
+
+// Close menu on nav menu clicks
+
+// scroll to top functionality
+
+// Theme switcher functionality
